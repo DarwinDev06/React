0350b04237ec9940ad810a19f861b135e72295ea
DarwinDev06 <darwinnoel15@gmail.com>
2024-02-19 17:50:13 -0500

    componentizando la app y aplicacando buenas practicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,92 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import confetti from 'canvas-confetti'
 import './App.css'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/Winner'
 
+
+
+/* [Array(9).fill(null)] 'x','o','x','x','o','x','x','o','x',*/
 function App() {
-  return <h1>Triki</h1>
-  /* const [count, setCount] = useState(0)
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  //null -> hay ganador | false -> hay empate
+  const [winner, setWinner] = useState(null)
+
+  
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+    console.log('updating')
+    //no se actualiza esta posicion por que ya tiene algo
+    if(board[index] || winner) return
+    //acutalizamos tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    //cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //revisar si hay ganador
+    const newWiner = checkWinnerFrom(newBoard)
+    if(newWiner) {
+      confetti()
+      setWinner(newWiner) //ganador
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) //empate
+    }
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  ) */
+    <main className='board'>
+      <h1>Triki</h1>
+      <section className='game'>
+        {
+          board.map((square, index) => {
+            return (
+             <Square 
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+             >
+              {square}
+             </Square>
+            )
+          })
+        } 
+
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <section>
+        <button onClick={resetGame}>Empezar de nuevo</button>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner}/>
+
+    </main>
+    
+  )
+  
 }
 
 export default App
---
+++ src/logic/board.js
@@ -0,0 +1,25 @@
+import { WINNWE_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras 
+    //para verificar si X u O ganó
+    for(const combo of WINNWE_COMBOS) {
+      const [a,b,c] = combo
+
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
+
+  export const checkEndGame = (newBoard) => {
+    //revisamos si hay empate
+    //si hay mas espacios vacios en el tablero
+    return newBoard.every((square) => square !== null)
+  }
